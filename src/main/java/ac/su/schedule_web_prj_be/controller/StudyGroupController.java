87b5800ee7819f99e4e58b216fbd9182ec0cf77a
ash <ash_tensor@icloud.com>
2024-07-09 19:19:34 +0900

    initial commitment

---
+++ src/main/java/ac/su/schedule_web_prj_be/controller/StudyGroupController.java
@@ -0,0 +1,4 @@
+package ac.su.schedule_web_prj_be.controller;
+
+public class StudyGroupController {
+}
---
+++ src/main/java/ac/su/schedule_web_prj_be/service/StudyGroupService.java
@@ -0,0 +1,33 @@
+package ac.su.schedule_web_prj_be.service;
+
+import ac.su.schedule_web_prj_be.domain.Member;
+import ac.su.schedule_web_prj_be.domain.StudyGroup;
+import ac.su.schedule_web_prj_be.repository.StudyGroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class StudyGroupService {
+    private final StudyGroupRepository studyGroupRepository;
+    public StudyGroup buildStudyGroup(String name, String description, List<Member> members, int memberCount) {
+        StudyGroup studyGroup = new StudyGroup();
+        studyGroup.setCreated_at(new Date());
+        studyGroup.setDescription(description);
+        studyGroup.setName(name);
+        studyGroup.setMembers(members);
+        studyGroup.setMemberCount(memberCount);
+    }
+
+    public StudyGroup joinStudyGroup(Long id, Member member) {
+        StudyGroup studyGroup = studyGroupRepository.findByStudyGroupId(id).get();
+        List<Member> memberList = studyGroup.getMembers();
+        memberList.add(member);
+        studyGroup.setMembers(memberList);
+        studyGroupRepository.updateStudyGroupMembersById(id, memberList);
+
+    }
+}

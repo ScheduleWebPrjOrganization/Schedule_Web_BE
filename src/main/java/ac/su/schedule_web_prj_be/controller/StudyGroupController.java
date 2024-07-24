752b9606b572831f6b44b95882a3c83c591031d3
msaki <msaki061@gmail.com>
2024-07-24 22:21:53 +0900

    get 500 에러 해결 (final 안붙임) + 불필요한 주석 제거

--- src/main/java/ac/su/schedule_web_prj_be/controller/StudyGroupController.java
+++ src/main/java/ac/su/schedule_web_prj_be/controller/StudyGroupController.java
@@ -3,11 +3,8 @@ package ac.su.schedule_web_prj_be.controller;
 import ac.su.schedule_web_prj_be.domain.StudyGroup;
 import ac.su.schedule_web_prj_be.service.StudyGroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
